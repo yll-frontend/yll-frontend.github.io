5e4a94a5e748c2ac085203f89740f6d61356be3b
lilin.yang <lilin.yang@infaith.com.cn>
2021-07-23 08:14:46 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 const head = require('./config/head.js');
 const plugins = require('./config/plugins.js');
 const themeConfig = require('./config/themeConfig.js');
-
+const imgBaseUrl = "https://sls-cloudfunction-ap-shanghai-code-1305386406.cos.ap-shanghai.myqcloud.com/"
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
   title: "Yang ll's Blog ",
@@ -10,7 +10,7 @@ module.exports = {
   markdown: {
     lineNumbers: true, // 代码行号
   },
-
+  imgBaseUrl,
   head,
   plugins,
   themeConfig,
--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -5,5 +5,5 @@ export default ({
   router, // 当前应用的路由实例
   siteData // 站点元数据
 }) => {
-  // window.Vue = vue // 使页面中可以使用Vue构造函数 （使页面中的vue demo生效）
+  // window.Vue = vue
 }
